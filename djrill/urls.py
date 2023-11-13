64af5a7c54c8acb2aebe9281d080a534824d0a2b
Sam Kuehn <samkuehn@gmail.com>
2023-11-13 10:01:29 -0700

    Fix urls

--- djrill/urls.py
+++ djrill/urls.py
@@ -1,14 +1,7 @@
-try:
-    from django.urls import re_path as url
-except ImportError:
-    try:
-        from django.conf.urls import url
-    except ImportError:
-        from django.conf.urls.defaults import url
+from django.urls import path
 
 from .views import DjrillWebhookView
 
-
 urlpatterns = [
-    url(r'^webhook/$', DjrillWebhookView.as_view(), name='djrill_webhook'),
+    path("webhook/", DjrillWebhookView.as_view(), name="djrill_webhook"),
 ]
